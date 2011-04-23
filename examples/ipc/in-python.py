db803ce58c3360376493333612683a7a410a0773
Nick Bargnesi <nick@den-4.com>
2011-04-22 22:17:13 -0400

    Send sys.argv[1] as IPC message.

--- examples/ipc/in-python.py
+++ examples/ipc/in-python.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
 from socket import *
-import os, os.path
+import os, os.path, sys
+
+if len(sys.argv) != 2:
+    print 'usage:', sys.argv[0], '<IPC_COMMAND>'
+    sys.exit(1)
 
 sckt = socket(AF_UNIX, SOCK_DGRAM)
 sckt.connect('/home/nick/.xy/ipc')
-
-sckt.send("babye")
+sckt.send(sys.argv[1])
