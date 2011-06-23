6db250c84c435f72030182ef793d931737a3971c
Nick Bargnesi <nick@den-4.com>
2011-06-23 01:18:19 -0400

    Documentation.

--- src/event.h
+++ src/event.h
@@ -22,44 +22,120 @@
 
 /*
  * Function: event_handler
+ *
+ * Invokes the appropriate xy callback.
+ *
+ * Parameters:
+ *
+ *  XEvent * - pointer to an XEvent
  */
 void event_handler(XEvent *);
 
 /*
  * Function: is_control_pressed
+ *
+ * Returns true if control is pressed in the key event, false otherwise.
+ *
+ * Parameters:
+ *
+ *  XKeyEvent * - pointer to XKeyEvent
+ *
+ * Returns:
+ *
+ *  bool
  */
 bool is_control_pressed(XKeyEvent *);
 
 /*
  * Function: is_shift_pressed
+ *
+ * Returns true if shift is pressed in the key event, false otherwise.
+ *
+ * Parameters:
+ *
+ *  XKeyEvent * - pointer to XKeyEvent
+ *
+ * Returns:
+ *  
+ *  bool
  */
 bool is_shift_pressed(XKeyEvent *);
 
 /*
  * Function: is_mod1_pressed
+ *
+ * Returns true if Mod1 is pressed in the key event, false otherwise.
+ *
+ * Parameters:
+ *
+ *  XKeyEvent * - pointer to XKeyEvent
+ *
+ * Returns:
+ *
+ *  bool
  */
 bool is_mod1_pressed(XKeyEvent *);
 
 /*
  * Function: is_mod2_pressed
+ *
+ * Returns true if Mod2 is pressed in the key event, false otherwise.
+ *
+ * Parameters:
+ *
+ *  XKeyEvent * - pointer to XKeyEvent
+ *
+ * Returns:
+ *
+ *  bool
  */
 bool is_mod2_pressed(XKeyEvent *);
 
 /*
  * Function: is_mod3_pressed
+ * 
+ * Returns true if Mod3 is pressed in the key event, false otherwise.
+ *
+ * Parameters:
+ *
+ *  XKeyEvent * - pointer to XKeyEvent
+ *
+ * Returns:
+ *
+ *  bool
  */
 bool is_mod3_pressed(XKeyEvent *);
 
 /*
  * Function: is_mod4_pressed
+ *
+ * Returns true if Mod4 is pressed in the key event, false otherwise.
+ *
+ * Parameters:
+ *
+ *  XKeyEvent * - pointer to XKeyEvent
+ *
+ * Returns:
+ *
+ *  bool
  */
 bool is_mod4_pressed(XKeyEvent *);
 
 /*
  * Function: is_ks_pressed
  *
- * Returns *true* if the key shortcut is being pressed in the key event,
- * *false* otherwise.
+ * Returns true if the key shortcut is being pressed in the key event,
+ * false otherwise.
+ *
+ * Parameters:
+ *
+ *  const char * - the key shortcut
+ *  XKeyEvent * - pointer to XKeyEvent
+ *
+ * Returns:
+ *
+ *  bool
+ *
  */
 bool is_ks_pressed(const char *, XKeyEvent *);
 
